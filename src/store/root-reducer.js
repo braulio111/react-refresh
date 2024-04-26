e42874a96ce743150f5f97cdb05c32ca9ec76b31
Braulio De La Garza <brauliodelagarza11@gmail.com>
2024-04-25 22:32:49 -0600

    implement redux persist

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+
 import { cartReducer } from "./cart/cart.reducer";
 import { categoriesReducer } from "./categories/category.reducer";
 import { userReducer } from "./user/user.reducer";
